052058266bea73a2285c5b02ea19e14ce6cd2c4e
Tom Zabek <38759734+tzabek@users.noreply.github.com>
2025-05-13 08:16:17 +0100

    fix: refactor (#9)

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as SidePanel } from './Sidebar/SidePanel';
 export { default as Sudoku } from './Sudoku/Sudoku';
 export { default as SudokuCell } from './SudokuCell/SudokuCell';
 export { default as ActionButton } from './Sidebar/ActionButton';
+export { default as Progress } from './Sidebar/Progress';
